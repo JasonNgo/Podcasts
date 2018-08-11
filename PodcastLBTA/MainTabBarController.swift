8c7977d4e8636b4e3494dca240626ab38db2c65c
Jason Ngo <jasonnngo@gmail.com>
2018-08-11 15:57:57 -0400

    setup skeleton layout for tabBarController

---
+++ PodcastLBTA/MainTabBarController.swift
@@ -0,0 +1,50 @@
+//
+//  MainTabBarController.swift
+//  PodcastLBTA
+//
+//  Created by Jason Ngo on 2018-08-11.
+//  Copyright © 2018 Jason Ngo. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupControllerStyles()
+        setupTabBarControllers()
+    }
+    
+    // MARK: - Setup Functions
+    
+    func setupControllerStyles() {
+        view.backgroundColor = .white
+        tabBar.tintColor = .purple
+        UINavigationBar.appearance().prefersLargeTitles = true
+    }
+    
+    func setupTabBarControllers() {
+        viewControllers = [
+            createNavigationController(for: ViewController(), title: "Favourites", image: #imageLiteral(resourceName: "favourite")),
+            createNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            createNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    // MARK: - Helper Functions
+    
+    fileprivate func createNavigationController(for rootViewController: ViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        
+        rootViewController.navigationItem.title = title
+        
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image.withRenderingMode(.alwaysOriginal)
+        
+        return navController
+    }
+    
+    
+} // MainTabBarController
